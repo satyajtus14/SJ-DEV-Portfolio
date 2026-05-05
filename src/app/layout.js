2dc0ce303d610356bc6d7789df2eea2357f609c5
“satyajitus14” <sjbarua14@gmail.com>
2026-05-05 00:11:12 -0400

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "SATYAJIT'S Portfolio",
+  description: "Web Developer focused on building responsive, high-quality web applications using modern JavaScript tools. Passionate about clean code, performance, and great user experience ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <main>
+
+        {children}
+        </main>
+        
+        </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>SATYAJIT BARUA Portfolio</h1>
+
+    </div>
+  );
+}
